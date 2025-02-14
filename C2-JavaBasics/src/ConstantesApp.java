6198cad81f96638654aaedde324c7f250a762514
Mauzner <candalija97@gmail.com>
2025-02-14 11:01:23 +0100

    Create ConstantesApp.java

---
+++ C2-JavaBasics/src/ConstantesApp.java
@@ -0,0 +1,14 @@
+
+public class ConstantesApp {
+
+	public static void main(String[] args) {
+		
+		final double IVA = 0.18;
+		int producto = 300;
+		
+		double resultado = producto*IVA;
+		
+		System.out.println("El IVA del producto es " +resultado);
+	}
+
+}
