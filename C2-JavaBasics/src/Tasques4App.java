afa3142bc8d357061b635cfbe2374046b6f19697
Mauzner <candalija97@gmail.com>
2025-02-17 11:47:58 +0100

    Tasques4-1

---
+++ C2-JavaBasics/src/Tasques4App.java
@@ -0,0 +1,20 @@
+
+public class Tasques4App {
+
+	    public static void main(String[] args) {
+	        // Declaramos las dos variables numéricas
+	        int a = 10;
+	        int b = 5;
+
+	        // Realizamos las operaciones y mostramos los resultados
+	        System.out.println("Suma: " + (a + b));
+	        System.out.println("Resta: " + (a - b));
+	        System.out.println("Multiplicación: " + (a * b));
+	        System.out.println("División: " + (a / b));
+	        System.out.println("Módulo (resto de la división): " + (a % b));
+	    
+
+
+	}
+
+}
