e90ba637383a98450b0ea1b2866cc80ea90e46c1
Mauzner <candalija97@gmail.com>
2025-02-17 12:25:04 +0100

    Create UD4ejercicio4App.java

---
+++ C2-JavaBasics/src/UD4ejercicio4App.java
@@ -0,0 +1,24 @@
+
+public class UD4ejercicio4App {
+
+	    public static void main(String[] args) {
+	        // Declaramos la variable N y le asignamos un valor
+	        int N = 1;
+
+	        // Mostramos el valor inicial de N
+	        System.out.println("Valor inicial de N = " + N);
+
+	        // Incrementamos N en 77
+	        N += 77;
+	        System.out.println("N + 77 = " + N);
+
+	        // Decrementamos N en 3
+	        N -= 3;
+	        System.out.println("N - 3 = " + N);
+
+	        // Doblamos el valor de N
+	        N *= 2;
+	        System.out.println("N * 2 = " + N);
+	    }
+	}
+
