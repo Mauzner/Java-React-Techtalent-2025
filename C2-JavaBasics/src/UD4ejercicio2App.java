e145c4eb665f4eb17c5eb2e89a330d293019c7bc
Mauzner <candalija97@gmail.com>
2025-02-17 11:58:01 +0100

    Create UD4ejercicio2App.java

---
+++ C2-JavaBasics/src/UD4ejercicio2App.java
@@ -0,0 +1,20 @@
+
+public class UD4ejercicio2App {
+
+	    public static void main(String[] args) {
+	        // Declaramos las variables
+	        int N = 5;
+	        double A = 4.56;
+	        char C = 'a';
+
+	        // Mostramos los valores de las variables
+	        System.out.println("Variable N = " + N);
+	        System.out.println("Variable A = " + A);
+	        System.out.println("Variable C = " + C);
+
+	        // Realizamos las operaciones y mostramos los resultados
+	        System.out.println(N + " + " + A + " = " + (N + A));
+	        System.out.println(A + " - " + N + " = " + (A - N));
+	        System.out.println("Valor numérico del carácter " + C + " = " + (int) C);
+	    }
+	}
