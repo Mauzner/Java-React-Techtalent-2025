36beee3e7ec6d9a6f6b015dcfc8d5544058de834
Mauzner <candalija97@gmail.com>
2025-02-18 12:34:46 +0100

    Create UD5ejercicio2.java

---
+++ C2-JavaBasics/src/UD05FlujoDatos/UD5ejercicio2.java
@@ -0,0 +1,13 @@
+package UD05FlujoDatos;
+
+public class UD5ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		String nombre = "Marc";
+	
+		System.out.println( "Bienvenido" + nombre);
+		
+	}
+
+}
