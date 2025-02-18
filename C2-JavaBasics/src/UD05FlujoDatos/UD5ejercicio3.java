58e7639538842fb5f80cf23eab10013f892ec699
Mauzner <candalija97@gmail.com>
2025-02-18 12:54:27 +0100

    Create UD5ejercicio3.java

---
+++ C2-JavaBasics/src/UD05FlujoDatos/UD5ejercicio3.java
@@ -0,0 +1,14 @@
+package UD05FlujoDatos;
+
+import javax.swing.JOptionPane;
+
+public class UD5ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		String nombre = JOptionPane.showInputDialog(" Introduce tu nombre ");
+		
+		JOptionPane.showMessageDialog( null, " Bienvenido " + nombre);
+	}
+
+}
