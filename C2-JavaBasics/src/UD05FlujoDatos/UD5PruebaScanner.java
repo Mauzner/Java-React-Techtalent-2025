5a17388118bdb876b0eddbd79a01addcd6d4e1dc
Mauzner <candalija97@gmail.com>
2025-02-18 13:21:53 +0100

    Create UD5PruebaScanner.java

---
+++ C2-JavaBasics/src/UD05FlujoDatos/UD5PruebaScanner.java
@@ -0,0 +1,22 @@
+
+package UD05FlujoDatos;
+
+import java.util.Scanner;
+
+public class UD5PruebaScanner {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner( System.in );
+		
+		System.out.println( " -- Bienvenida a la consulta --");
+		System.out.println( " Indroduce tus sintomas: ");
+		
+		String name = sc.nextLine();
+		sc.close();
+		
+		System.out.println( " Me la pela tu " + name.toLowerCase());
+
+	}
+
+}
