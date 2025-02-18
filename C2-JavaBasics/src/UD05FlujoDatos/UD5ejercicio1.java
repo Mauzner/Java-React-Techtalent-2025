9988670ae915a6ec72e7185d4b97aeae3c460cff
Mauzner <candalija97@gmail.com>
2025-02-18 12:02:31 +0100

    Create UD5ejercicio1.java

---
+++ C2-JavaBasics/src/UD05FlujoDatos/UD5ejercicio1.java
@@ -0,0 +1,19 @@
+package UD05FlujoDatos;
+
+public class UD5ejercicio1 {
+
+	public static void main(String[] args) {
+		int num1 = 5;
+		int num2 = 5;
+		
+		if(num1 > num2) {
+			System.out.println( num1 + " es mayor que" + num2);
+		} else if (num1 < num2) {
+			System.out.println( num2 +" es mayor que" + num1);
+		} else {
+			System.out.println( num1 + " y " + num2 + " son iguales");
+		}
+
+	}
+
+}
