47c1f6e800974cd63678f3d70d1aff819eabc321
Mauzner <candalija97@gmail.com>
2025-02-18 13:33:46 +0100

    Create UD5PruebaJOptionPane.java

---
+++ C2-JavaBasics/src/UD05FlujoDatos/UD5PruebaJOptionPane.java
@@ -0,0 +1,17 @@
+package UD05FlujoDatos;
+
+import javax.swing.JOptionPane;
+
+public class UD5PruebaJOptionPane {
+
+	public static void main(String[] args) {
+		
+		int precioProducto = 1000;
+		
+		final double IVA = 0.21;
+		
+		JOptionPane.showMessageDialog (null, "El precio de producto, incluido el IVA es " + (precioProducto + (precioProducto * IVA)));
+
+	}
+
+}
