9f8d0054b8807adae9ba4d1b14a36985d403d68e
Mauzner <candalija97@gmail.com>
2025-02-21 12:34:20 +0100

    Create UD5ejercicio4.java

---
+++ C2-JavaBasics/src/UD05FlujoDatos/UD5ejercicio4.java
@@ -0,0 +1,24 @@
+package UD05FlujoDatos;
+
+import java.util.Scanner;
+
+public class UD5ejercicio4 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in); 
+		
+		System.out.println(" Introduce el radio el círculo: ");
+		String input = scanner.nextLine();
+		
+		double radio = Double.parseDouble(input);
+		
+		double area = Math.PI * Math.pow(radio, 2);
+		
+		System.out.println(" El area del circulo es: " + area);
+		
+		scanner.close();	
+
+	}
+
+}
