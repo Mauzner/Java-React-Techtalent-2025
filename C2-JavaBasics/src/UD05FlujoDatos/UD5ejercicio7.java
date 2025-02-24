c73a5438fb4ec249157b943923195e9df16d63d4
Mauzner <candalija97@gmail.com>
2025-02-24 13:24:57 +0100

    UD5ejercicio6/7

---
+++ C2-JavaBasics/src/UD05FlujoDatos/UD5ejercicio7.java
@@ -0,0 +1,15 @@
+package UD05FlujoDatos;
+
+public class UD5ejercicio7 {
+
+	public static void main(String[] args) {
+		// Muestra los números del 1 al 100 (ambos incluidos). Usa un bucle while.
+		int i = 1;
+		while (i <= 100) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
