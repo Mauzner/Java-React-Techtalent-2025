1af253c61845ce30a5aecea02a148c41bc48669c
Mauzner <candalija97@gmail.com>
2025-02-25 10:53:03 +0100

    Update UD5ejercicio10.java

--- C2-JavaBasics/src/UD05FlujoDatos/UD5ejercicio10.java
+++ C2-JavaBasics/src/UD05FlujoDatos/UD5ejercicio10.java
@@ -1,9 +1,27 @@
 package UD05FlujoDatos;
 
+import java.util.Scanner;
+
 public class UD5ejercicio10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(" Ingrese el número de ventas ");
+		int numVentas = scanner.nextInt();
+		
+		double totalVentas = 0;
+		
+		for (int i = 1; i <= numVentas; i++) {
+			System.out.println("Ingrese el valor de la venta " + i);
+			double valorVenta = scanner.nextDouble();
+
+			totalVentas += valorVenta;
+		}
+		
+		System.out.println("El total de ventas es: " + totalVentas);
+		
+		scanner.close();
 
 	}
 
