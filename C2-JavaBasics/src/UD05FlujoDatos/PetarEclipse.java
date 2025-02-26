5e4fcf1aebaa74275fc072fbe893b32e67088524
Mauzner <candalija97@gmail.com>
2025-02-26 11:04:02 +0100

    Create PetarEclipse.java

---
+++ C2-JavaBasics/src/UD05FlujoDatos/PetarEclipse.java
@@ -0,0 +1,19 @@
+package UD05FlujoDatos;
+
+
+
+public class PetarEclipse {
+
+	public static void main(String[] args) {
+
+		int num=0;
+		String num3 ="";
+		while(true) {
+			num++;
+			num3+=num;
+			System.out.println(num3);
+		}
+
+	}
+
+}
