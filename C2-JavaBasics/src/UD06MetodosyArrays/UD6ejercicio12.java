af423e052115cc0d49cde9a2d61b65da262e7bf2
Mauzner <candalija97@gmail.com>
2025-03-12 08:44:35 +0100

    Create UD6ejercicio12.java

---
+++ C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio12.java
@@ -0,0 +1,20 @@
+package UD06MetodosyArrays;
+
+import java.util.Scanner;
+
+public class UD6ejercicio12 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int size = pedirSize(sc);
+		int digito = pedirDigito(sc);
+		
+		
+		
+		
+		
+	}
+
+}
