8012b3d063270a1baea9d45338c7f5cc27d6960e
Mauzner <candalija97@gmail.com>
2025-03-14 13:36:59 +0100

    edit

--- C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio06.java
+++ C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio06.java
@@ -1,13 +1,40 @@
 package UD06MetodosyArrays;
 
+import java.util.Scanner;
 
 public class UD6ejercicio06 {
 
 	public static void main(String[] args) {
 		
-
+		Scanner scanner = new Scanner(System.in);
+		
+		//Pedir al usuario un núnmero enetro positivo
+		System.out.println("Introduce un número entero positivo: ");
+		int num = scanner.nextInt();
+		
+		//Verificar que el número sea positivo
+		if (num <= 0) {
+			System.out.println("El número introducido no es positivo.");
+		} else {
+			// Llamar al método contarCifras y mostrar el resultado
+			System.out.println("El número " + num + " tiene " + contarCifras(num) + " cifras.");
+		}
+		
+		scanner.close();
 	}
 	
+	public static int contarCifras(int num) {
+		int contador = 0;
+
+		// Mientras el número sea mayor que 0
+		while (num > 0) {
+			// Dividir el número entre 10
+			num = num / 10;
+			// Aumentar el contador de cifras
+			contador++;
+		}
+		return contador;
+	}
 }
 
 
--- C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio07.java
+++ C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio07.java
@@ -1,9 +1,17 @@
 package UD06MetodosyArrays;
 
+import java.util.Scanner;
+
 public class UD6ejercicio07 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String divisa="";
+		int valor=0;
+		
+		System.out.println("Pasa un valor en euros" );
+		valor = valorConsole.nextInt();
+		valor
 
 	}
 
