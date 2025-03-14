5c46c16c9e2a6245577d2e7a32ec8ae392998a44
Mauzner <candalija97@gmail.com>
2025-03-14 13:54:32 +0100

    Update UD6ejercicio07.java

--- C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio07.java
+++ C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio07.java
@@ -5,14 +5,60 @@ import java.util.Scanner;
 public class UD6ejercicio07 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String divisa="";
-		int valor=0;
-		
-		System.out.println("Pasa un valor en euros" );
-		valor = valorConsole.nextInt();
-		valor
 
-	}
+
+			Scanner valorConsole = new Scanner(System.in);
+			String divisa = "";
+			int valor = 0;
+
+			System.out.println("Pasa un valor en euros:");
+			valor = valorConsole.nextInt();
+			valorConsole.nextLine(); 
+			
+			do {
+				System.out.println("Indica a quina divisa vols convertir:");
+				System.out.println("libras");
+				System.out.println("dolares");
+				System.out.println("yenes"); 
+				divisa = valorConsole.nextLine();
+				divisa = divisa.toLowerCase();
+			} while (!divisa.equals("libras") 
+					 && !divisa.equals("dolares") 
+					 && !divisa.equals("yenes"));
+
+			
+			switch (divisa) {
+				case "libras":
+					convertirEuroLibras(valor);
+					break;
+				case "dolares":
+					convertirEuroDolares(valor);
+					break;
+				case "yenes":
+					convertirEuroYenes(valor);
+					break;
+				default:
+					System.out.println("Divisa no reconocida.");
+					break;
+			}
+			System.out.println("Valor actual a  14/03/2025");
+			valorConsole.close();
+
+		}
+
+		private static void convertirEuroLibras(int valor) {
+			System.out.println("La conversió de " + valor + " € són £" + (valor * 0.84) 
+					+ " (libras)");
+		}
+
+		private static void convertirEuroDolares(int valor) {
+			System.out.println("La conversió de " + valor + " € són $" + (valor * 1.09) 
+					+ " (dólares)");
+		}
+
+		private static void convertirEuroYenes(int valor) {
+			System.out.println("La conversió de " + valor + " € són ¥" + (valor * 162.09) 
+					+ " (yenes)");
+		}
 
 }
