27d072a53aa37f845692ae3f509c2a22fd008ff9
Mauzner <candalija97@gmail.com>
2025-03-18 10:58:01 +0100

    Update UD6ejercicio08.java

--- C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio08.java
+++ C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio08.java
@@ -1,10 +1,36 @@
 package UD06MetodosyArrays;
 
+import java.util.Scanner;
+
 public class UD6ejercicio08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int size = 10;
+		int [] miArray= llenarArray(size);
+		imprimirArray(miArray);
+		
 	}
-
+	
+	public static int[] llenarArray(int size) {
+		Scanner scanner = new Scanner(System.in);
+		int[] array = new int[size];
+		for (int i = 0; i < size; i++) {
+			System.out.print("Introduce un número: ");
+			array[i] = scanner.nextInt();
+		}
+		return array;
+		
+	}
+	
+	
+	public static void imprimirArray (int[] array) {
+		System.out.println( " \nValores del array: ");
+		for (int num : array) {
+			System.out.println(num + " ");
+		}
+	}
+	
 }
+		 
+		 
