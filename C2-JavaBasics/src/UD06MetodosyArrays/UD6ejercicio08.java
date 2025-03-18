6b21fa9a7ab05de875641748f9da542226ff1043
Mauzner <candalija97@gmail.com>
2025-03-18 11:52:02 +0100

    Update UD6ejercicio08.java

--- C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio08.java
+++ C2-JavaBasics/src/UD06MetodosyArrays/UD6ejercicio08.java
@@ -10,6 +10,7 @@ public class UD6ejercicio08 {
 		int [] miArray= llenarArray(size);
 		imprimirArray(miArray);
 		
+		
 	}
 	
 	public static int[] llenarArray(int size) {
@@ -18,10 +19,13 @@ public class UD6ejercicio08 {
 		for (int i = 0; i < size; i++) {
 			System.out.print("Introduce un número: ");
 			array[i] = scanner.nextInt();
+			
+			
 		}
 		return array;
 		
 	}
+	
 	
 	
 	public static void imprimirArray (int[] array) {
