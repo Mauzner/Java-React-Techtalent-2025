85c8689a1b3b35e4adc63f8ce5a00837705533bd
Mauzner <candalija97@gmail.com>
2025-03-19 13:37:14 +0100

    iuo

--- C2-JavaBasics/src/UD07ArrayListHashMap/UD7ejercicio03.java
+++ C2-JavaBasics/src/UD07ArrayListHashMap/UD7ejercicio03.java
@@ -1,10 +1,35 @@
 package UD07ArrayListHashMap;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class UD7ejercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Introduce el nombre del producto: ");
+		
+		HashMap< String, Integer> producto = new HashMap<>(10);
+				
+		producto.put("cerveza", 2);
+		producto.put("agua", 1);
+		producto.put("vino", 7);
+		producto.put("refresco", 3);
+		producto.put("lechuga", 1);
+		producto.put("tomate", 1);
+		producto.put("pan", 2);
+		producto.put("carne", 5);
+		producto.put("pescado", 6);
+		producto.put("queso", 4);
+		
+		
+		
+		
+		
+		 
+		
 	}
 
 }
