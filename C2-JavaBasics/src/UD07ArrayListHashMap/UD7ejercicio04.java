cc33444fdf3969520630bd0258a7877b0f583fc5
Mauzner <candalija97@gmail.com>
2025-03-26 08:54:12 +0100

    Update UD7ejercicio04.java

--- C2-JavaBasics/src/UD07ArrayListHashMap/UD7ejercicio04.java
+++ C2-JavaBasics/src/UD07ArrayListHashMap/UD7ejercicio04.java
@@ -6,18 +6,7 @@ public class UD7ejercicio04 {
 
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
-		49p849op34fe
-		f
-		4f4
-		55g
-		5g
-		5g5
-		g5
-		g5g
-		5r
-		
-		scanner.close();
+	
 
 	}
 
