eab37dd81221bf3f72c0955d510de15e392c6e39
Mauzner <candalija97@gmail.com>
2025-04-07 09:38:47 +0200

    Update UD8ejercicio01.java

--- C2-JavaBasics/src/UD08Poo/UD8ejercicio01.java
+++ C2-JavaBasics/src/UD08Poo/UD8ejercicio01.java
@@ -8,15 +8,69 @@ public class UD8ejercicio01 {
 	private char sexo;
 	private double peso;
 	private double altura;
+	
+	public static final char SEXO_HOMBRE = 'H';
 
 	UD8ejercicio01() {
 		this.nombre = "";
 		this.edad = 0;
-		this.dni = "";
-		this.sexo = 'M';
-		this.peso = 75.0;
-		this.altura = 1.80;
+		this.dni = " Sin dni";
+		this.sexo = SEXO_HOMBRE;
+		this.peso = 0;
+		this.altura = 0;
 	}
+	
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	public double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+	
+	
 
 	UD8ejercicio01(String nombre, int edad, String dni, char sexo, double peso, double altura) {
 		this.nombre = nombre;
