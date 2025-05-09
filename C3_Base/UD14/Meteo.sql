3c129cb3c9b8752e05ab6172339529030fb19086
Mauzner <candalija97@gmail.com>
2025-05-09 13:46:55 +0200

    ud14

---
+++ C3_Base/UD14/Meteo.sql
@@ -0,0 +1,20 @@
+CREATE TABLE estacion (
+    id MEDIUMINT UNSIGNED NOT NULL,
+    lat VARCHAR(14) NOT NULL,
+    longitud VARCHAR(15) NOT NULL,
+    alt MEDIUMINT NOT NULL,
+    PRIMARY KEY (id)   
+);
+
+CREATE TABLE muestra (
+    idestación MEDIUMINT UNSIGNED NOT NULL,
+    fecha DATE NOT NULL,
+    tempmin TINYINT,
+    tempmax TINYINT,
+    precipita SMALLINT UNSIGNED,
+    hummmin TINYINT UNSIGNED,
+    hummax TINYINT UNSIGNED,
+    velmin SMALLINT UNSIGNED,
+    velmax SMALLINT UNSIGNED,
+    KEY (idestacion), FOREIGN KEY (idestacion) REFERENCES estacion(id) ON DELETE NO ACTION ON UPDATE CASCADE
+);
