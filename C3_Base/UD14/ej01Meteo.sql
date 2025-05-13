7adf1316bb480dc8812762fa747291d3d1db723e
Mauzner <candalija97@gmail.com>
2025-05-13 12:39:31 +0200

    Update ej01Meteo.sql

--- C3_Base/UD14/ej01Meteo.sql
+++ C3_Base/UD14/ej01Meteo.sql
@@ -2,22 +2,20 @@ CREATE DATABASE meteo;
 USE meteo;
 
 CREATE TABLE estacion (
-    idestacion MEDIUMINT UNSIGNED NOT NULL,
+    id MEDIUMINT UNSIGNED NOT NULL,
     lat VARCHAR(14) NOT NULL,
     longitud VARCHAR(15) NOT NULL,
     alt MEDIUMINT NOT NULL,
-    PRIMARY KEY (id)   
-);
+    PRIMARY KEY (id));
 
 CREATE TABLE muestra (
-    idestación MEDIUMINT UNSIGNED NOT NULL,
+    idestacion MEDIUMINT UNSIGNED NOT NULL,
     fecha DATE NOT NULL,
     tempmin TINYINT,
     tempmax TINYINT,
     precipita SMALLINT UNSIGNED,
-    hummmin TINYINT UNSIGNED,
+    hummin TINYINT UNSIGNED,
     hummax TINYINT UNSIGNED,
     velmin SMALLINT UNSIGNED,
     velmax SMALLINT UNSIGNED,
-    PRIMARY KEY (idestacion), FOREIGN KEY (idestacion) REFERENCES estacion(id) ON DELETE NO ACTION ON UPDATE CASCADE
-);
+    KEY (idestacion), FOREIGN KEY (idestacion) REFERENCES estacion (id) ON DELETE NO ACTION ON UPDATE CASCADE);
