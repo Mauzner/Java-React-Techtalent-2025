2c284f60007bd14b307a0012bf2084f56d4e8816
Mauzner <candalija97@gmail.com>
2025-05-16 12:22:24 +0200

    finish

--- C3_Base/UD14/ej12academia.sql
+++ C3_Base/UD14/ej12academia.sql
@@ -1,18 +1,47 @@
-CREATE DATABASE academia;
-USE academia;
+CREATE DATABASE academia
+
+USE academia
 
 CREATE TABLE profesores(
-    dni VARCHAR (9);
-    nombre VARCHAR(255),
-    apellido1 VARCHAR(255),
-    apellido2 VARCHAR(255),
+    dni CHAR(9) NOT NULL,
+    nombre VARCHAR (100),
+    apellido1 VARCHAR(100),
+    apellido2 VARCHAR(100),
+    CONSTRAINT unique_nombre UNIQUE (nombre, apellido1, apellido2),
     direccion VARCHAR(100),
-    titulacion VARCHAR(50),
-    sueldo SMALLINT NOT NULL,
-    UNIQUE (nnombre, apellido1,apellido2),
-    PRIMARY KEY (dni)
-);
+    titulo VARCHAR(100),
+    gana INT NOT NULL,
+    PRIMARY KEY (dni));
 
+CREATE TABLE cursos(
+    id_curso INT NOT NULL,
+    nombre VARCHAR(100),
+    dni_profesor CHAR(9),
+    max_alumnos INT,
+    fecha_inicio DATE,
+    fecha_fin DATE,
+    CONSTRAINT fecha_crono CHECK (fecha_inicio < fecha_fin),
+    num_horas INT NOT NULL,
+    UNIQUE (nombre),
+    PRIMARY KEY (id_curso),
+    FOREIGN KEY (dni_profesor) REFERENCES profesores (dni)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE);
+
+CREATE TABLE alumnos(
+    dni CHAR(9) NOT NULL,
+    nombre VARCHAR (100),
+    apellido1 VARCHAR(100),
+    apellido2 VARCHAR(100),
+    direccion VARCHAR(100),
+    sexo CHAR(1),
+    CONSTRAINT sexo_alumno CHECK (sexo IN ('H', 'M')),
+    fecha_nacimiento DATE,
+    curso INT NOT NULL,
+    PRIMARY KEY (dni),
+    FOREIGN KEY (curso) REFERENCES cursos(id_curso)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE);
 CREATE TABLE cursos(
     cod INT,
     nombre VARCHAR(100),
